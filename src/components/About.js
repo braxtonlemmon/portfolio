0208a1f0618f519545b9978fc89d775aecf74ef5
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-02-19 18:07:30 +0100

    Add icons to About and Contact pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { 
+  FaHtml5,
+  FaCss3Alt,
+  FaGitAlt,
+  FaReact,
+  FaLinux,
+  FaNpm
+} from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
+import { Icon } from '@iconify/react';
+import jestIcon from '@iconify/icons-cib/jest';
+import webpackIcon from '@iconify/icons-cib/webpack';
+import languageRubyOnRails from '@iconify/icons-mdi/language-ruby-on-rails';
+import eslinticon from '@iconify/icons-mdi/eslint';
+import prettierIcon from '@iconify/icons-cib/prettier';
+import rubyIcon from '@iconify/icons-cib/ruby';
+import postgresqlIcon from '@iconify/icons-cib/postgresql';
 
 class About extends Component {
   render() {
-    return <h1>About</h1>;
+    return (
+      <div>
+        <FaHtml5 size={"2em"} />
+        <FaCss3Alt size={"2em"} />
+        <FaGitAlt size={"2em"} />
+        <FaReact size={"2em"} />
+        <FaLinux size={"2em"} />
+        <IoLogoJavascript size={"2em"} />
+        <FaNpm size={"2em"} />
+        <Icon icon={jestIcon} height={"2em"} />
+        <Icon icon={webpackIcon} height={"2em"} color="black" />
+        <Icon icon={languageRubyOnRails} height={"2em"} color="black" />
+        <Icon icon={eslinticon} height={"2em"} color="black" />
+        <Icon icon={prettierIcon} height={"2em"} />
+        <Icon icon={rubyIcon} height={"2em"} />
+        <Icon icon={postgresqlIcon} height={"2em"} />
+      </div>
+    )
   }
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { FiMail } from 'react-icons/fi';
+import { 
+  FaLinkedinIn, 
+  FaGithub, 
+  FaTwitter, 
+  FaWhatsapp,
+  FaPhone,
+  FaSoundcloud,
+  FaFacebookMessenger
+} from 'react-icons/fa';
 
+const Icon = styled.a`
+  text-decoration: none;
+  color: black;
+`
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h3>Email:</h3>
-        <a href="mailto:braxtonlemmon@gmail.com">braxtonlemmon@gmail.com</a>
-        <h3>Phone:</h3>
-        <span>+32 493 12 33 08</span>
+        <Icon href="https://linkedin.com/in/braxtonlemmon/">
+          <FaLinkedinIn size={"2em"} />
+        </Icon>
+        <Icon href="https://github.com/braxtonlemmon">
+          <FaGithub size={"2em"} />
+        </Icon>
+        <Icon href="https://twitter.com/braxtonlemmon">
+          <FaTwitter size={"2em"} />
+        </Icon>
+        <Icon href="https://wa.me/32493123308">
+          <FaWhatsapp size={"2em"} />
+        </Icon>
+        <Icon href="tel:+32493123308">
+          <FaPhone size={"2em"} />
+        </Icon>
+        <Icon href="mailto:braxtonlemmon@gmail.com">
+          <FiMail size={"2em"} />
+        </Icon>
+        <Icon href="https://soundcloud.com/expiredmomentum">
+          <FaSoundcloud size={"2em"} />
+        </Icon>
+        <Icon href="https://m.me/braxton.lemmon">
+          <FaFacebookMessenger size={"2em"} />
+        </Icon>
       </div>
     );
   }
