57f5f1ac227f08f1fb33902985db5b94047b4495
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-02-28 13:50:37 +0100

    Extract hobby data to separate file.

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const hobbyList = [
-  { name: 'rock climbing', img: 'climbing.jpeg' },
-  { name: 'cooking',       img: 'cooking.png' },
-  { name: 'making music',  img: 'makingmusic.jpg' },
-  { name: 'camping',       img: 'camping.jpeg' },
-  { name: 'homebrewing',   img: 'brewing.jpg' },
-  { name: 'reading',       img: 'reading.webp' },
-  { name: 'board games',   img: 'boardgames.jpg' },  
-]
-
-const generateHobbies = () => {
-  return hobbyList.map(hobby => {
-    return (
-      <Hobby key={hobby} imgUrl={process.env.PUBLIC_URL + `img/${hobby.img}`}>
-        {hobby.name}
-      </Hobby>
-    )
-  })
-}
+import hobbyList from '../data/HobbyData.js';
 
 const Hobby = styled.span`
   padding: 8px;
@@ -33,17 +14,27 @@ const Hobby = styled.span`
     background-position: center;
     color: transparent;
   }
-`
+`;
 
 const HobbiesBox = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  
+
   @media only screen and (min-width: 43em) {
     width: 70%;
   }
-`
+`;
+
+const generateHobbies = () => {
+  return hobbyList.map(hobby => {
+    return (
+      <Hobby key={hobby} imgUrl={process.env.PUBLIC_URL + `img/${hobby.img}`}>
+        {hobby.name}
+      </Hobby>
+    )
+  })
+}
 
 const Hobbies = () => {
   return (
---
+++ src/data/HobbyData.js
@@ -0,0 +1,11 @@
+const hobbyList = [
+  { name: "rock climbing", img: "climbing.jpeg" },
+  { name: "cooking", img: "cooking.png" },
+  { name: "making music", img: "makingmusic.jpg" },
+  { name: "camping", img: "camping.jpeg" },
+  { name: "homebrewing", img: "brewing.jpg" },
+  { name: "reading", img: "reading.webp" },
+  { name: "board games", img: "boardgames.jpg" }
+];
+
+export default hobbyList;
