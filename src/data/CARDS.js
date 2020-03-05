fa597a5deedae8481afde6b410d6018e42e55269
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-05 11:14:37 +0100

    Remove banana

--- src/data/CARDS.js
+++ src/data/CARDS.js
@@ -1,82 +1,88 @@
 const CARDS = [
-  { id: 0, 
-    img: 'odinbook.gif', 
-    github: 'https://github.com/braxtonlemmon/odin-facebook',
-    live: 'https://murmuring-wave-95625.herokuapp.com/',
+  {
+    id: 0,
+    img: "odinbook.gif",
+    github: "https://github.com/braxtonlemmon/odin-facebook",
+    live: "https://murmuring-wave-95625.herokuapp.com/",
     isLive: true,
     x: 0,
     y: 0,
     info: {
-      title: 'The Odin Book',
-      about: 'Social networking project',
-      technologies: ['Ruby on Rails', 'HTML', 'CSS'],
-    },
+      title: "The Odin Book",
+      about: "Social networking project",
+      technologies: ["Ruby on Rails", "HTML", "CSS"]
+    }
   },
-  { id: 1, 
-    img: 'chess.gif', 
-    github: 'https://github.com/braxtonlemmon/chess',
-    live: '',
+  {
+    id: 1,
+    img: "chess.gif",
+    github: "https://github.com/braxtonlemmon/chess",
+    live: "",
     isLive: false,
     x: 0,
     y: 0,
     info: {
-      title: 'Chess',
-      about: 'Command line chess game',
-      technologies: ['Ruby'],
-    },
+      title: "Chess",
+      about: "Command line chess game",
+      technologies: ["Ruby"]
+    }
   },
-  { id: 2, 
-    img: 'pomodoro.gif', 
-    github: 'https://github.com/braxtonlemmon/pomodoro',
-    live: 'https://braxtonlemmon.github.io/pomodoro/',
+  {
+    id: 2,
+    img: "pomodoro.gif",
+    github: "https://github.com/braxtonlemmon/pomodoro",
+    live: "https://braxtonlemmon.github.io/pomodoro/",
     isLive: true,
-    x: '-15px',
+    x: "-15px",
     y: 0,
     info: {
-      title: 'Pomodoro Timer',
-      about: 'Productivity tool to manage workflow',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-    },
+      title: "Pomodoro Timer",
+      about: "Productivity tool to manage workflow",
+      technologies: ["HTML", "CSS", "JavaScript"]
+    }
   },
-  { id: 3, 
-    img: 'weather.gif', 
-    github: 'https://github.com/braxtonlemmon/weather-app',
-    live: 'https://braxtonlemmon.github.io/weather-app/',
+  {
+    id: 3,
+    img: "weather.gif",
+    github: "https://github.com/braxtonlemmon/weather-app",
+    live: "https://braxtonlemmon.github.io/weather-app/",
     isLive: true,
-    x: '-10px',
+    x: "-10px",
     y: 0,
     info: {
-      title: 'Weather Finder',
-      about: 'Uses OpenWeather API to obtain live weather data',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'API'],
-    },
+      title: "Weather Finder",
+      about: "Uses OpenWeather API to obtain live weather data",
+      technologies: ["HTML", "CSS", "JavaScript", "React", "API"]
+    }
   },
-  { id: 4, 
-    img: 'pitchfork.gif', 
-    github: 'https://github.com/braxtonlemmon/pitchfork-clone',
-    live: 'https://braxtonlemmon.github.io/pitchfork-clone/',
+  {
+    id: 4,
+    img: "pomodoro.gif",
+    github: "https://github.com/braxtonlemmon/pitchfork-clone",
+    live: "https://braxtonlemmon.github.io/pitchfork-clone/",
     isLive: true,
-    x: '-10px',
+    x: "-10px",
     y: 0,
     info: {
-      title: 'Pitchfork Clone',
-      about: 'Visual replication of pitchfork.com',
-      technologies: ['HTML', 'CSS'],
-    },
+      title: "Pitchfork Clone",
+      about: "Visual replication of pitchfork.com",
+      technologies: ["HTML", "CSS"]
+    }
   },
-  { id: 5, 
-    img: 'pitchfork.gif',
-    github: 'https://github.com/braxtonlemmon/pitchfork-clone',
-    live: 'https://braxtonlemmon.github.io/pitchfork-clone/',
+  {
+    id: 5,
+    img: "pomodoro.gif",
+    github: "https://github.com/braxtonlemmon/pitchfork-clone",
+    live: "https://braxtonlemmon.github.io/pitchfork-clone/",
     isLive: true,
     x: 0,
     y: 0,
     info: {
-      title: 'Pitchfork Clone',
-      about: 'Visual replication of pitchfork.com',
-      technologies: ['HTML', 'CSS'],
-    },
-  } 
+      title: "Pitchfork Clone",
+      about: "Visual replication of pitchfork.com",
+      technologies: ["HTML", "CSS"]
+    }
+  }
 ];
 
 export default CARDS;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from 'react-router-dom';
-import "typeface-bungee-shade";
 import "typeface-acme"
 
 // if (process.env.NODE_ENV === 'development') {
@@ -21,7 +19,3 @@ import "typeface-acme"
     </Router>, 
     document.getElementById("root"));
 
-
-
-
-serviceWorker.unregister();
